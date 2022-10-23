1cf372d2610022db4cca2b36bd562f74dcb2cc12
Prashant Mokani <prashant.mokani@ekzero.com>
2022-10-23 19:33:37 +0530

    initial commit

---
+++ src/main/java/com/prashant/dcms/api/DcmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.prashant.dcms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcmsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prashant/dcms/api/DcmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashant.dcms.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcmsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
